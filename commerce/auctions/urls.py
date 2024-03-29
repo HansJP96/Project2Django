9b628a6e2b948456212dff05265aa0e4c72b1e44
Hansee <hansee1996@gmail.com>
2024-03-29 11:46:23 -0500

    Avance front end auction view

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("listings/<int:id_auction>", views.auction, name="auction")
 ]
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "auctions/index.html")
+
+
+def auction(request, id_auction):
+    return render(request, "auctions/auction_view.html")
